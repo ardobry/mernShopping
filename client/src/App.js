421d839e5ca25f992c44c8002e4a6b937df4cf20
Peace4Every1 <paligyan@gmail.com>
2019-05-04 13:49:22 +0400

    ShoppingList component & transitions added

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import AppNavbar from './components/AppNavbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
   return (
     <div className="App">
       <AppNavbar/>
+      <ShoppingList/>
     </div>
   );
 }
