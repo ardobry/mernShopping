0e178c3f914fc4cd6035cde962f057c4bbf7180c
Peace4Every1 <paligyan@gmail.com>
2019-05-05 13:50:16 +0400

    Deploy preparation

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 2.Concurrently - for running more than 1 npm script at a time, analog for having 2 terminals.
 3."client": "npm start --prefix client" - for running npm start from client folder
 3.1. cd client && npm start - would also do the same thing
+4.heroku postproduction - is a build preparation, it's not gonna build, if it's in production
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const items = require('./routes/api/items');
-
+const path = require('path');
 
 
 const app = express();
@@ -22,6 +22,16 @@ mongoose.connect(db)
 //I want anything that goes to /api/items/* to go to that file
 app.use('/api/items', items);
 
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => { 
