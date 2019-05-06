2bbce321796522cdd12622fd5bfa1f710b2b9356
Peace4Every1 <paligyan@gmail.com>
2019-05-06 16:18:17 +0400

    Actions & reducers for redux auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import AppNavbar from "./components/AppNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ShoppingList from "./components/ShoppingList";
 import { Provider } from "react-redux";
 import store from "./store";
-import ItemModal from './components/ItemModal';
-import {Container} from 'reactstrap'
+import ItemModal from "./components/ItemModal";
+import { Container } from "reactstrap";
+import { loadUser } from "./actions/authActions";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <AppNavbar />
-        <Container>
-          <ItemModal />
-          <ShoppingList />
-        </Container>
-      </div>
-    </Provider>
-  );
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
